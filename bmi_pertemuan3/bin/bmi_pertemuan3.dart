056ed5585f137396065a1dbf0d05bf6cc48b80ca
jadennatha <144076853+jadennatha@users.noreply.github.com>
2025-09-18 14:26:36 +0700

    commit project dart

---
+++ bmi_pertemuan3/bin/bmi_pertemuan3.dart
@@ -0,0 +1,5 @@
+import 'package:bmi_pertemuan3/bmi_pertemuan3.dart' as bmi_pertemuan3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bmi_pertemuan3.calculate()}!');
+}
---
+++ bmi_pertemuan3/test/bmi_pertemuan3_test.dart
@@ -0,0 +1,8 @@
+import 'package:bmi_pertemuan3/bmi_pertemuan3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
